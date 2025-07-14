ea0763bfa35f4d5ab5c9fced6ef5a8c6137770d7
amit7321 <amit.hasan7321@gmail.com>
2025-07-15 01:50:38 +0600

    Initialize the project.

---
+++ src/main/java/com/example/Spring_Boot_School_Project/SpringBootSchoolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_School_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSchoolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSchoolProjectApplication.class, args);
+	}
+
+}
